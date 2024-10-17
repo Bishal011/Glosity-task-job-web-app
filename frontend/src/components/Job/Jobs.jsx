3ea275b7cabd5ef38e48aa9b6336c8c863dbfdd3
BISHAL PRADHAN <105370000+Bishal011@users.noreply.github.com>
2024-10-17 20:37:53 +0530

    Update Jobs.jsx

--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -10,7 +10,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("http://localhost:4000/api/v1/job/getall", {
+        .get("https://glosity-task-job-web-app-14.onrender.com/api/v1/job/getall", {
           withCredentials: true,
         })
         .then((res) => {
