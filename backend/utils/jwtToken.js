6934eb2a66e27fba1cb57518a9a63cbb460865b5
BISHAL PRADHAN <105370000+Bishal011@users.noreply.github.com>
2024-10-18 22:39:35 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,6 +5,8 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true, // Set httpOnly to true
+    secure: true, // Send cookie only over HTTPS in production
+    sameSite: "None", // Allows cross-origin cookies in production
   };
 
   res.status(statusCode).cookie("token", token, options).json({
